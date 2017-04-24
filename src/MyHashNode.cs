908ab4ae886f8b8185e50b61863431a5dc032123
pholland08 <bulldog105@gmail.com>
2017-04-24 03:17:39 -0500

    test

---
+++ src/MyHashNode.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashingLab.src
+{
+    class MyHashNode
+    {
+        #region Fields{get; set;}
+        // TODO Declare field: HashKey
+        private string _HashKey;
+        public string HashKey
+        {
+            get { return _HashKey; }
+            set { _HashKey = value; }
+        }
+
+        // TODO Declare field: InitialAddress
+        private int _InitialAddress;
+        public int InitialAddress
+        {
+            get { return _InitialAddress; }
+            set { _InitialAddress = value; }
+        }
+
+        // TODO Declare field: CurrentLocation
+        private int _CurrentLocation;
+        public int CurrentLocation
+        {
+            get { return _CurrentLocation; }
+            set { _CurrentLocation = value; }
+        }
+
+        // TODO Declare field: ProbeCount
+        private int _ProbeCount;
+        public int ProbeCount
+        {
+            get { return _ProbeCount; }
+            set { _ProbeCount = value; }
+        }
+        #endregion Fields
+
+        #region Constructors
+        ////////////////////////////////////////////////////////////////////
+        /// Constructors
+        ////////////////////////////////////////////////////////////////////
+        // No argument constructor
+        internal MyHashNode()
+        {
+            this.InitialAddress = -1;
+            this.CurrentLocation = -1;
+            this.ProbeCount = 0;
+            this.HashKey = null;
+        }
+
+        // Default constructor
+        public MyHashNode(string key) : this()
+        {
+            this.HashKey = key;
+            this.InitialAddress = HashFunctions.CalculateHash(key);
+        }
+        #endregion Constructors
+
+        #region Methods
+        // TODO Write method ToString()
+        public override string ToString()
+        {
+            return $"{HashKey}\t{InitialAddress}\t{CurrentLocation}\t{ProbeCount}";
+        }
+        #endregion Methods
+    }
+}
---
+++ src/Program.cs
@@ -0,0 +1,68 @@
+﻿using HashingLab.src;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HashingLab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // TODO Open file for reading
+            string[] lines = System.IO.File.ReadAllLines("./../../input/words.txt");
+            //string[] lines = System.IO.File.ReadAllLines("./../../input/phil.txt");
+
+            // Create or erase output file
+            File.Create("./../../input/output.txt").Close();
+            StreamWriter file = new System.IO.StreamWriter("./../../input/output.txt", true);
+
+            // TODO Initialize hash table
+            MyHashTable table = new MyHashTable(128);
+
+            // While less than 40% full, add entries to table
+            int CurrentLine = 0;
+            //while (table.CurrentCapacity < (table.Length * .4d)) { table.Add(lines[CurrentLine++]); }
+            while (table.CurrentCapacity < (table.Length * .99d)) { table.Add(lines[CurrentLine++]); }
+
+            // TODO Get min, max, and avg #probes for the first 30 words
+            CurrentLine = 0;
+            int ProbeSum = 0;
+            int ProbeMin = table.Length;
+            int ProbeMax = -1;
+            while (CurrentLine < table.CurrentCapacity)
+            {
+                MyHashNode retrieved = table.Get(lines[CurrentLine]);
+                ProbeSum += retrieved.ProbeCount;
+                ProbeMin = retrieved.ProbeCount < ProbeMin ? retrieved.ProbeCount : ProbeMin;
+                ProbeMax = retrieved.ProbeCount > ProbeMax ? retrieved.ProbeCount : ProbeMax;
+                CurrentLine++;
+            }
+            // TODO Get min, max, and avg #probes for the last 30 words
+            
+            // TODO Print table
+            Console.WriteLine(table.ToString());
+            file.Write(table.ToString());
+
+            // 
+            file.WriteLine($"Minimum number of probes: {ProbeMin}");
+            file.WriteLine($"Maximum number of probes: {ProbeMax}");
+            file.WriteLine($"average number of probes: {(double)ProbeSum / table.CurrentCapacity}");
+            file.Close();
+            Console.WriteLine($"Minimum number of probes: {ProbeMin}");
+            Console.WriteLine($"Maximum number of probes: {ProbeMax}");
+            Console.WriteLine($"average number of probes: {(double)ProbeSum / table.CurrentCapacity}");
+            double a = (double)table.CurrentCapacity / (double)table.Length;
+            double E = (1 - a / 2) / (1 - a);
+            Console.WriteLine($"Load factor (alpha): {a}");
+            Console.WriteLine($"Expected number of probes: {E}");
+
+            // End program
+            Console.ReadKey();
+            Environment.Exit(0);
+        }
+    }
+}
