87e8659a76effa4cb4f64118f9d6893e1b48b10e
Phillip Holland <phillip.holland328@gmail.com>
2018-11-07 00:06:52 -0600

    Found missing changes.

---
+++ src/HashFunctions.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace HashingLab.src
+{
+    class HashFunctions
+    {
+        // Write method CalculateHash
+        public static int CalculateHash(string key, string algorithm, int PrimeTS)
+        {
+            // Instantiate return value
+            int _address = 1;
+
+            // Perform given hash algorithm
+            switch (algorithm)
+            {
+                case "burris":
+                    {
+                        char[] _KeyChars = key.ToCharArray();
+                        int first = Convert.ToInt32(string.Format("{0}{1}", (int)_KeyChars[0], (int)_KeyChars[1]));
+                        int second = Convert.ToInt32(string.Format("{0}{1}", (int)_KeyChars[5], (int)_KeyChars[6]));
+                        _address = (((first + second) * 256) + (int)_KeyChars[12]) % 128;
+                        break;
+                    }
+                default:
+                    {
+                        char[] _KeyChars = key.ToCharArray();
+
+                        int first = Convert.ToInt32(string.Format("{0}{1}", (int)_KeyChars[1] + 2, (int)_KeyChars[2] + 3));
+                        int second = Convert.ToInt32(string.Format("{0}{1}", (int)_KeyChars[5] + 6, (int)_KeyChars[6] + 7));
+                        int third = Convert.ToInt32(string.Format("{0}{1}", (int)_KeyChars[1] + 2, (int)_KeyChars[6] + 7));
+                        int fourth = Convert.ToInt32(string.Format("{0}{1}", (int)_KeyChars[5] + 6, (int)_KeyChars[2] + 3));
+
+                        _address = Math.Abs((first ^ second) + (third ^ fourth));
+
+                        foreach (char k in _KeyChars)
+                        {
+                            _address = ((_address * 2) + (int)k) % PrimeTS;
+                        }
+                        break;
+                    }
+            }
+            return _address;
+        }
+    }
+}
---
+++ src/MyRandomAccesFile.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+namespace HashingLab.src
+{
+    public class MyRandomAccesFile
+    {
+        #region Fields
+        // File to read and write from
+        private FileStream _RandFile;
+        private int _DataOffset;
+        #endregion Fields
+
+        #region Constructors
+        private MyRandomAccesFile() { }
+        public MyRandomAccesFile(string filepath) : this()
+        {
+            _RandFile = new FileStream(filepath, FileMode.Create, FileAccess.ReadWrite);
+        }
+        public MyRandomAccesFile(String filepath, int DataOffset, int Length) : this(filepath)
+        {
+            _DataOffset = DataOffset;
+            byte[] blanks = new byte[DataOffset * Length];
+            _RandFile.Write(blanks, 0, DataOffset * Length);
+        }
+        #endregion Constructors
+
+        #region Methods
+        // Write read method
+        public string RandomRead(int Record)
+        {
+            string retrieved;
+
+            // Position stream pointer
+            int _Record = Record * _DataOffset;
+            int NumBytes = ((Record + 1) * _DataOffset) - _Record;
+            SeekOrigin Begin = default(SeekOrigin);
+            _RandFile.Seek(_Record, Begin);
+
+            // Prepare buffer
+            byte[] buffer = new byte[NumBytes];
+
+            // Read and return string
+            int bytes = _RandFile.Read(buffer, 0, buffer.Length);
+            retrieved = Encoding.ASCII.GetString(buffer, 0, bytes);
+            return retrieved.Split('#')[0];
+        }
+
+        // Write write method
+        public bool RandomWrite(int Record, string obj)
+        {
+            // Prepare buffer
+            byte[] buffer = Encoding.ASCII.GetBytes(obj += "#");
+
+            // Position stream pointer
+            int _Record = Record * _DataOffset;
+            SeekOrigin Begin = default(SeekOrigin);
+            _RandFile.Seek(_Record, Begin);
+
+            // Write to file and flush
+            _RandFile.Write(buffer, 0, buffer.Length);
+            _RandFile.Flush();
+            return false;
+        }
+
+        // write method close
+        public void Close()
+        {
+            _RandFile.Flush();
+            _RandFile.Close();
+        }
+    }
+    #endregion Methods
+}
